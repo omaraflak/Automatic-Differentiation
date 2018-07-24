2db73a24953fd83ec673de8e01176a82376aa52c
Omar Aflak <aflakomar@gmail.com>
2018-07-24 23:14:35 +0200

    added some comments

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,12 @@
 #include "../include/sgd.h"
 
 Dual function(std::vector<Dual> x){
-    return x[0]*x[0]+x[1]*x[1];
+    return x[0]*x[0]+x[1]*x[1]; // x^2 + y^2
 }
 
 int main() {
-    std::vector<Dual> x = {Dual(50), Dual(50)};
-    SGD::decrease(function, x, 10, 0.7);
+    std::vector<Dual> x = {Dual(50), Dual(50)}; // 50 are the default values
+    SGD::decrease(function, x, 10, 0.7); // 10 iterations, 0.7 learning rate
     std::cout << "f = " << function(x) << std::endl;
     for(unsigned int i=0 ; i<x.size() ; i++){
         std::cout << "x[" << i << "] = " << x[i] << std::endl;
