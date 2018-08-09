3830c0c0ee0b8f155d7dbbacc4b4815aac2bf822
Omar Aflak <aflakomar@gmail.com>
2018-08-09 20:08:53 +0200

    renamed files

--- root/src/main.cpp
+++ root/src/main.cpp
@@ -1,19 +1,39 @@
 #include <iostream>
 #include "../include/dual.h"
-#include "../include/tools.h"
+#include "../include/gradient.h"
 
-Dual function(std::vector<Dual> x){
-    return x[0]*x[0]+x[1]*x[1]; // x^2 + y^2
+typedef std::vector<Dual> Vector;
+
+std::ostream& operator<<(std::ostream& os, const Vector& v){
+    os << "[";
+    for(size_t i=0 ; i<v.size() ; i++){
+        os << v[i] << (i<v.size()-1?", ":"");
+    }
+    os << "]";
+    return os;
+}
+
+Dual function(Vector& x){
+    return x[0]*x[0]+x[1]*x[1]; // f(x1,x2) = x1^2 + x2^2
 }
 
 int main() {
-    std::vector<Dual> x = {Dual(50), Dual(50)}; // 50 are the default values
-    SGD::decrease(function, x, 10, 0.7); // 10 iterations, 0.7 learning rate
+    Vector x = {50, 70}; // random default values
 
-    // print function and parameters
-    std::cout << "f = " << function(x) << std::endl;
-    for(unsigned int i=0 ; i<x.size() ; i++){
-        std::cout << "x[" << i << "] = " << x[i] << std::endl;
+    // hyperparameters
+    int epochs = 30;
+    float learningRate = 0.3;
+
+    // gradient descent
+    for(size_t i=0 ; i<epochs ; i++){
+        Vector gradient = dual::gradient(function, x);
+        for(size_t j=0 ; j<x.size() ; j++){
+            x[j] -= learningRate*gradient[j];
+        }
     }
+
+    // check new values
+    std::cout << "x = " << x << std::endl;
+    std::cout << "f(x) = " << function(x) << std::endl;
     return 0;
 }
